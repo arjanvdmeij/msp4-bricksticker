e46eb1c79f7c8eb2496b51cbad4b04c05ec58fa4
Arjan van der Meij <a.vandermeij@gmail.com>
2019-02-04 14:42:52 +0000

    Created a filter for categories alongside regular search option

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import search
+from .views import search, filter_products
 
 urlpatterns = [
-    url(r'^$', search, name='search'),
+    url(r'^$', filter_products, name='filter_products'),
+    url(r'^search/$', search, name='search'),
     ]
--- search/views.py
+++ search/views.py
@@ -5,12 +5,23 @@ from products.models import Product
 # Create your views here.
 def search(request):
     products = Product.objects.filter(
-        Q(description__icontains=request.GET['q']) | Q(category__icontains=request.GET['q'])
+        Q(description__icontains=request.GET['q']) | 
+        Q(category__icontains=request.GET['q'])
         )
-    categories = Product.objects.values('category').order_by('category').distinct().filter(
-        Q(description__icontains=request.GET['q']) | Q(category__icontains=request.GET['q'])
-        )
-    return render(request, 'products.html', {'products':products,
+    categories = Product.objects.values(
+        'category').order_by('category').distinct()
+    return render(request, 'products.html', 
+        {'products':products,
         'categories':categories,
-        
+    })
+
+def filter_products(request):
+    products = Product.objects.filter(
+        Q(category__startswith=request.GET['q'])
+        )
+    categories = Product.objects.values(
+        'category').order_by('category').distinct()
+    return render(request, 'products.html', 
+        {'products':products,
+        'categories':categories,
     })

96a3bf15a90e349999a5eb7b8e2a60b5dcfb53d8
Arjan van der Meij <a.vandermeij@gmail.com>
2019-02-06 17:41:20 +0000

    Made Cloud 9 dev environment look at local locations for static and media files.

--- bricksticker/settings.py
+++ bricksticker/settings.py
@@ -154,34 +154,46 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2219 23:59:59 GMT',
-    'CacheControl': 'max-age=94608000',
-}
-AWS_STORAGE_BUCKET_NAME = 'bss-msp-4'
-AWS_S3_REGION_NAME = 'eu-west-1'
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-STATIC_URL = '/static/'
-STATICFILES_DIRS =  (
-    os.path.join(BASE_DIR, "static"),
-    )
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-    )
+if os.getenv('C9_HOSTNAME'):
+    print('Using local static and media locations')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS =  (
+        os.path.join(BASE_DIR, "static"),
+        )
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+        
+        
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+else:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2219 23:59:59 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    AWS_STORAGE_BUCKET_NAME = 'bss-msp-4'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-MEDIA_URL = 'https://%s/%s/' % (
-    AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media'),
-    )
+    STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS =  (
+        os.path.join(BASE_DIR, "static"),
+        )
+    STATIC_ROOT = (
+        os.path.join(BASE_DIR, 'staticfiles'),
+        )
+        
+    MEDIAFILES_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIA_URL = 'https://%s/%s/' % (
+        AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+    MEDIA_ROOT = (
+        os.path.join(BASE_DIR, 'media'),
+        )
 
 STRIPE_PK = os.getenv('STRIPE_PK')
 STRIPE_SK = os.getenv('STRIPE_SK')

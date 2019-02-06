29ed0c9266f934d4e62c311de8e882396ed4e6c6
Arjan van der Meij <a.vandermeij@gmail.com>
2019-02-06 17:19:23 +0000

    Modified test to reflect correct response

--- products/tests.py
+++ products/tests.py
@@ -3,5 +3,9 @@ from .models import Product
 
 class ProductTests(TestCase):
     def test_str(self):
-        test_name = Product(name='Service Station')
-        self.assertEqual(str(test_name), 'Service Station')
+        test_name = Product(
+            setnumber = '3681',
+            name = 'Amusement Park',
+            category = 'Fabuland',
+            )
+        self.assertEqual(str(test_name), '3681 - Amusement Park - Fabuland')

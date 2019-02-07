349b8c82ecb720139494fd56d3bbe725ddb9a0e1
Arjan van der Meij <a.vandermeij@gmail.com>
2019-02-07 12:57:53 +0000

    tweaked urls

--- products/tests.py
+++ products/tests.py
@@ -10,15 +10,13 @@ class ProductTests(TestCase):
             )
         self.assertEqual(str(product), '42 - Zephod - Beeblebrox')
     
+    def test_get_home_page(self):
+        page = self.client.get('/')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'index.html')
+    
     def test_get_products_page(self):
         page = self.client.get('/products/products/')
         print(page)
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, 'products.html')
-    
-    def test_get_home_page(self):
-        page = self.client.get('/')
-        self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, 'index.html')
-        
-    

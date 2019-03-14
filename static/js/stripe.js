232e23c0f9f9d519a64eab8005a4bf72dc543888
Arjan van der Meij <a.vandermeij@gmail.com>
2019-03-14 10:22:44 +0000

    changed commenting to larger block on top of document, with more explanation

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,5 +1,17 @@
 /* global $ */
 
+/**********************************************************************************
+ * Modifications were made to require all fields in form.
+ * As a result, clearing the form as the original stripe.js did, breaks things.
+ * Instead, Fields will now be returned to server, but cleared of user data
+ * Expiration month and year are set 15+ years into the future
+ * Card Number and CVV are changed to fields of asterisks
+ * 
+ * This is an intermediate solution until the upgrade to Stripev3 is done
+ * at which point the stripe.js file becomes unnecessary
+ * 
+ **********************************************************************************/
+
 $(function() {
     $("#payment-form").submit(function() {
       var form = this;
@@ -16,11 +28,9 @@ $(function() {
           $("#id_stripe_id").val(response.id);
           
           // Prevent the Credit Card Details from being submitted to our server.
-          // ** Modification made to require all fields in form.
-          // ** Fields will be returned to server, but cleared of user data
           $("#id_credit_card_number").val('****************');
           $("#id_cvv").val('******');
-          // modify entered values for expiration to accepted near-max values
+          // ** modify entered values for expiration to accepted near-max values
           $("#id_expiry_month").css('color','white').val(12);
           $("#id_expiry_year").css('color','white').val(2035);
         
